0d641c789648342ad3581a2a8fd16cdd8345ea1d
Artjom Smorgulenko <artjomsmorgulenko@gmail.com>
2026-04-22 20:36:12 +0300

    Application, candidate models, applicationRoute, database connection

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const noteRouter = require("./routes/note");
-
+const mongoose = require("mongoose");
+const applicationRouter = require("./routes/applicationRoute");
 
 // Config
 dotenv.config();
 const port = process.env.PORT || 5000;
+const uri = process.env.URI;
+
+// connecting to db
+mongoose.connect(uri)
+.then((result) => console.log('connected to db'))
+.catch((err) => console.log(err));
 
 const app = express();
 app.use(express.json());
@@ -14,6 +21,7 @@ app.use(express.urlencoded());
 
 // Setup Routes
 app.use("/api/application", noteRouter);
+app.use("/api/application", applicationRouter);
 
 
 // Start server
