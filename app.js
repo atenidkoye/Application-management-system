46e562b83e25c4f678223215d85028fca8493c88
Your Name <atenidkoyea@gmail.com>
2026-04-22 19:45:52 +0300

    updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const hbs = require ("hbs");
+const cookieParser = require("cookie parser");
+const jwt = ("jsonwebtoken");
+const mongoose = require("mongoose");
 const noteRouter = require("./routes/note");
 
 
@@ -8,12 +12,50 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Middlewares
+app.use(cookieParser())
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"))
 
 
-// Setup Routes
+// View Engine
+app.set("view engine", "hbs");
+
+//Routes
 app.use("/api/application", noteRouter);
+app.use("/candidates", require("./routes/candaidateRoutes"));
+app.use("/applications", require("./routes/applicationRoutes"));
+app.use("/auth", require("./routes/authRoutes"));
+
+
+// Users Available in views
+app.use((req, res, next) =>{
+    const token = req.cookies.token;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            res.locals.user = decoded;
+        } catch{
+            res.locals.user = null;
+        }
+    } else {
+        res.locals.user = null;
+    }
+
+    next();
+})
+
+
+
+
+
+
+
+
+
 
 
 // Start server
