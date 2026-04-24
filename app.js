f7fb705e0791be335ecb1b1aff0066d71625282d
Samtastic01 <90844518+SamuelPasierb@users.noreply.github.com>
2026-04-24 11:59:47 +0300

    login and register views

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.use(express.static("public"))
 
 
 // Setting up handlebars
-app.engine("handlebars", exhbs.engine());
+app.engine("handlebars", exhbs.engine({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 //Routes
@@ -33,53 +35,68 @@ app.use("/api/application", applicationRouter);
 // app.use("/auth", require("./routes/authRoutes"));
 
 
-// // Users Available in views
-// app.use((req, res, next) =>{
-//     const token = req.cookies.token;
+// Check if user is already logged in
+app.use((req, res, next) =>{
+    const token = req.cookies.token;
 
-//     if (token) {
-//         try {
-//             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//             res.locals.user = decoded;
-//         } catch{
-//             res.locals.user = null;
-//         }
-//     } else {
-//         res.locals.user = null;
-//     }
-
-//     next();
-// });
-
-
-// // Home page
-// app.get("/", (req, res) => {
-//     res.redirect("/dashboard");
-// });
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            res.locals.user = decoded;
+        } catch{
+            res.locals.user = null;
+        }
+    } else {
+        res.locals.user = null;
+    }
+    next();
+});
 
 
-// //Dashboard
-// const auth = require("/middleware/auth");
-// const Candidate = require("/models/Candidate");
-// const auth = require("/models/Applications");
+// Home page
+app.get("/", (req, res) => {
+    if (res.locals.user) { // User logged in -> dashboard
+        res.redirect("/dashboard");
+    } else { // User is not logged in -> login page
+        res.redirect("/auth/login");
+    }
+});
 
-// app.get("/dashboard", auth, async(req, res, next) => {
-//     try{
-//         const totalCandidates = await Candidate.countDocuments();
+//Dashboard
+// const auth = require("./middleware/auth");
+// const Candidate = require("./models/Candidate");
 
-//         const stats = await application.aggregate([
-//             {$group: { _id: "$status", count: {$sum: 1}}}
-//         ]);
+app.get("/dashboard", async(req, res, next) => {
+    res.render("dashboard", {
+        style: "dashboard"
+    });
+    // try{
+    //     const totalCandidates = await Candidate.countDocuments();
 
-//         res.render("dashboard", {
-//             totalCandidates,
-//             statusStatus: stats,
-//             isDashboard: true
-//         });
-//     } catch (err) {
-//         next(err);
-//     }
-// });
+    //     const stats = await application.aggregate([
+    //         {$group: { _id: "$status", count: {$sum: 1}}}
+    //     ]);
+
+    //     res.render("dashboard", {
+    //         totalCandidates,
+    //         statusStatus: stats,
+    //         isDashboard: true
+    //     });
+    // } catch (err) {
+    //     next(err);
+    // }
+});
+
+app.get("/auth/:action", async(req, res, next) => {
+    const action = req.params.action;
+    if (action == "login" || action == "register") {
+        res.render(action, {
+            style: "auth"
+        });
+    } else {
+        res.status(404).json({msg: "The auth action was not found"})
+    }
+})
 
 // // Error handler
 // app.use(require("./middleware/errorHandler"));
